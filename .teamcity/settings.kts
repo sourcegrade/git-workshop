eed2590ecb97c6a1f2d23024c4a11e08f5993769
Alexander Städing <alexanderstaeding@gmail.com>
2024-02-02 14:10:54 +0100

    Revert "Add approval to teamcity conf"
    This reverts commit 40834ace70f7a2d69ef9f2a11e319d1e6642006e.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.BuildFeatures
 import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.DslContext
-import jetbrains.buildServer.configs.kotlin.buildFeatures.approval
 import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.exec
@@ -124,9 +123,5 @@ fun BuildFeatures.configureBaseFeatures() {
                 token = "credentialsJSON:f57a4fdd-fb30-41c0-9983-620364336d03"
             }
         }
-    }
-    approval {
-        approvalRules = "group:sourcegrade:1"
-        manualRunsApproved = true
     }
 }
